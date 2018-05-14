71e3a12baed9e50ee115b548059b14aee708afdf
Alexander Prada <aleprada_ar@hotmail.com>
2018-05-14 20:26:22 -0300

    Creacion de la clase Disco

--- src/paqueteClases/Disco.java
+++ src/paqueteClases/Disco.java
@@ -1,5 +1,51 @@
 package paqueteClases;
 
 public class Disco {
+	private Double radioInterior;
+	private Double radioExterior;
+	
+	public Disco() {
+		radioInterior = 0.0;
+		radioExterior = 0.0;
+	}
+	
+	public Disco(Double radioInterior,Double radioExterior) {
+		this.radioInterior = radioInterior;
+		this.radioExterior = radioExterior;
+	}
+	
+	public Double calcularPerimetroInterior() {
+		Double resultadoPerimetro;
+		resultadoPerimetro = 2 * Math.PI * radioInterior; 
+		return resultadoPerimetro;
+	}
 
+	public Double calcularPerimetroExterior() {
+		Double resultadoPerimetro;
+		resultadoPerimetro = 2 * Math.PI * radioExterior; 
+		return resultadoPerimetro;
+	}
+
+	public Double calcularSuperficie() {
+		Double resultadoSuperficie;
+		resultadoSuperficie = Math.sqrt(radioInterior) * Math.PI; 
+		return resultadoSuperficie;
+	}
+
+	public Double getRadioInterior() {
+		return radioInterior;
+	}
+
+	public void setRadioInterior(Double radioInterior) {
+		this.radioInterior = radioInterior;
+	}
+
+	public Double getRadioExterior() {
+		return radioExterior;
+	}
+
+	public void setRadioExterior(Double radioExterior) {
+		this.radioExterior = radioExterior;
+	}	
+	
 }
